ebef12b13b9e764b1f690e7bfedcc4de627698f3
aperwenis <artur.perwenis@persgroep.net>
2021-05-20 14:28:40 +0200

    remove git add from deploy

--- deploy_qa.sh
+++ deploy_qa.sh
@@ -74,9 +74,7 @@ if [[ ! $REPLY =~ ^[Yy](es)?$ ]]; then
 	exit 0
 fi
 
-echo "Committing..."
-git commit -m "Commit for tag $TAG"
-git push origin ${BRANCH_NAME}
+echo "Tagging..."
 git tag -a "$TAG" -m "$COMMENT"
 git push origin "$TAG"
 
